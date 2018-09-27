aba873cbe43e1d6be08e172fbb60c7f5d4fc97cc
Kwaku Twumasi-Afriyie <kwaku.twumasi@quakearts.com>
2018-09-27 12:17:15 +0000

    Add runtime exception type for errors that can't be handled

---
+++ src/main/java/com/mobiquityinc/exception/APIRuntimeException.java
@@ -0,0 +1,13 @@
+package com.mobiquityinc.exception;
+
+public class APIRuntimeException extends RuntimeException {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 8531773668510873691L;
+
+	public APIRuntimeException(String message, Throwable cause) {
+		super(message, cause);
+	}	
+}
