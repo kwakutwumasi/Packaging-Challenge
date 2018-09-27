d095cabafde63521cb2d0b0f27ff1894753c6e9b
Kwaku Twumasi-Afriyie <kwaku.twumasi@quakearts.com>
2018-09-27 14:57:44 +0000

    Check in packer file parser implementation

--- src/main/java/com/mobiquityinc/packer/fileparser/impl/PackerFileParserImpl.java
+++ src/main/java/com/mobiquityinc/packer/fileparser/impl/PackerFileParserImpl.java
@@ -1,18 +1,116 @@
 package com.mobiquityinc.packer.fileparser.impl;
 
+import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.text.MessageFormat;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 import com.mobiquityinc.exception.APIException;
 import com.mobiquityinc.packer.fileparser.PackerFileParser;
+import com.mobiquityinc.packer.i18n.Messages;
+import com.mobiquityinc.packer.model.PackageItem;
 import com.mobiquityinc.packer.model.PackerFileEntry;
 
 public class PackerFileParserImpl implements PackerFileParser {
 
+	private static final Map<String, Boolean> currencyMap = new HashMap<>();
+	static {
+		currencyMap.put("$", Boolean.TRUE);
+		currencyMap.put("€", Boolean.TRUE);
+	}
+	
 	@Override
 	public PackerFileEntry[] parse(InputStream inputStream) throws APIException, IOException {
-		// TODO Auto-generated method stub
-		return null;
+		BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
+		List<PackerFileEntry> entries = new ArrayList<>();
+		
+		if(entries.isEmpty())
+			throw new APIException(Messages.get("empty.file"));
+		
+		String line;
+		while ((line = reader.readLine())!=null) {
+			processLine(line, entries);
+		}
+		
+		return entries.toArray(new PackerFileEntry[entries.size()]);
+	}
+
+	private void processLine(String line, List<PackerFileEntry> entries) throws APIException {
+		String[] lineParts = line.split("\\s+:\\s+",2);
+		Double weightLimit; 
+		try {
+			weightLimit = Double.parseDouble(lineParts[0]);
+		} catch (NumberFormatException e) {
+			throw new APIException(MessageFormat.format(Messages.get("invalid.weight.limit"), lineParts[0]));
+		}
+		
+		if(lineParts.length!=2)
+			throw new APIException(MessageFormat.format(Messages.get("invalid.line"), line));
+		
+		PackerFileEntry entry = new PackerFileEntry().withWeightLimitAs(weightLimit);
+		entries.add(entry);
+		processEntries(entry, lineParts[2]);
+	}
+
+	private void processEntries(PackerFileEntry entry, String itemsString) throws APIException {
+		String[] items = itemsString.split("\\s+");
+		for(String item:items) {
+			if(item.startsWith("(")
+					&& item.endsWith(")")) {				
+				processEntry(entry,item.substring(1,item.length()-1));
+			} else {
+				throw new APIException(MessageFormat.format(Messages.get("invalid.item"), item));
+			}
+		}
+	}
+
+	private void processEntry(PackerFileEntry entry, String item) throws APIException {
+		String[] itemParts = item.split(",",3);
+		if(itemParts.length!=3)
+			throw new APIException(MessageFormat.format(Messages.get("invalid.item"), item));
+			
+		int index;
+		try {
+			index = Integer.parseInt(itemParts[0]);
+		} catch (NumberFormatException e) {
+			throw new APIException(MessageFormat.format(Messages.get("invalid.index"), itemParts[0]));
+		}
+		
+		double weight;
+		try {
+			weight = Double.parseDouble(itemParts[1]);
+		} catch (NumberFormatException e) {
+			throw new APIException(MessageFormat.format(Messages.get("invalid.weight"), itemParts[1]));
+		}
+
+		if(itemParts[2].length()<2) {
+			throw new APIException(MessageFormat.format(Messages.get("invalid.cost"), itemParts[2]));
+		}
+			
+		String currency = itemParts[2].substring(0, 1);
+		
+		if(!currencyMap.containsKey(currency))
+			throw new APIException(MessageFormat.format(Messages.get("invalid.currency"), currency));
+		
+		String costString = itemParts[2].substring(1);
+		
+		double cost;
+		try {
+			cost = Double.parseDouble(costString);
+		} catch (NumberFormatException e) {
+			throw new APIException(MessageFormat.format(Messages.get("invalid.cost"), costString));
+		}
+
+		entry.add(new PackageItem()
+				.withIndexAs(index)
+				.withWeightAs(weight)
+				.withCurrencyAs(currency)
+				.withCostAs(cost));
 	}
 
 }
