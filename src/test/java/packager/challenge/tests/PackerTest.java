3beceeb3a61c224ba23df6e70899c7fa3ae9be6d
Kwaku Twumasi-Afriyie <kwaku.twumasi@quakearts.com>
2018-09-27 08:17:32 +0000

    Packer Test initial test cases

--- src/test/java/packager/challenge/tests/PackerTest.java
+++ src/test/java/packager/challenge/tests/PackerTest.java
@@ -1,19 +1,42 @@
 package packager.challenge.tests;
 
 import static org.junit.Assert.*;
+import static org.hamcrest.core.Is.*;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.StringReader;
 
 import org.junit.Test;
+
+import com.mobiquityinc.packer.Packer;
 
 public class PackerTest {
 
 	@Test
-	public void testPackString() {
-		fail("Not yet implemented");
-	}
+	public void testPack() throws Exception {
+		String results = Packer.pack("src"+File.separator+"test"
+				+File.separator+"resources"
+				+File.separator+"test.parserfile");
 
-	@Test
-	public void testPackPackerFileEntry() {
-		fail("Not yet implemented");
+		BufferedReader bufferedReader = new BufferedReader(new StringReader(results));
+		assertThat(bufferedReader.readLine(), is("1"));
+		assertThat(bufferedReader.readLine(), is("_"));
+		assertThat(bufferedReader.readLine(), is("4"));
+		assertThat(bufferedReader.readLine(), is("0"));
+		assertThat(bufferedReader.readLine(), is("_"));
+		assertThat(bufferedReader.readLine(), is("2"));
+		assertThat(bufferedReader.readLine(), is("_"));
+		assertThat(bufferedReader.readLine(), is("2,7"));
+		assertThat(bufferedReader.readLine(), is("1"));
+		assertThat(bufferedReader.readLine(), is("_"));
+		assertThat(bufferedReader.readLine(), is("6"));
+		assertThat(bufferedReader.readLine(), is("0"));
+		assertThat(bufferedReader.readLine(), is("_"));
+		assertThat(bufferedReader.readLine(), is("4"));
+		assertThat(bufferedReader.readLine(), is("_"));
+		assertThat(bufferedReader.readLine(), is("1,2"));
+		assertThat(bufferedReader.readLine(), is("3,4"));
 	}
 
 }
