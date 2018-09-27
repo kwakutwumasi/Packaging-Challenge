9a9ea1a487096b971df9e5e50e5a1b0bb0e2d0ca
Kwaku Twumasi-Afriyie <kwaku.twumasi@quakearts.com>
2018-09-27 10:05:30 +0000

    Add package test cases

--- src/test/java/packager/challenge/tests/PackageTest.java
+++ src/test/java/packager/challenge/tests/PackageTest.java
@@ -1,24 +1,35 @@
 package packager.challenge.tests;
 
 import static org.junit.Assert.*;
+import static org.hamcrest.core.Is.*;
+
+import java.util.Arrays;
+
+import com.mobiquityinc.packer.model.Package;
+import com.mobiquityinc.packer.model.PackageItem;
 
 import org.junit.Test;
 
 public class PackageTest {
 
 	@Test
-	public void testGetTotalCost() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	public void testGetTotalWeight() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	public void testToString() {
-		fail("Not yet implemented");
+	public void testPackage() {
+		Package packageTest = new Package();
+		packageTest.addAll(Arrays.asList(new PackageItem().withCostAs(10d)
+				.withIndexAs(1)
+				.withWeightAs(20d),new PackageItem().withCostAs(11d)
+				.withIndexAs(2)
+				.withWeightAs(30d)));
+		
+		assertThat(packageTest.getTotalCost(), is(21d));
+		assertThat(packageTest.getTotalWeight(), is(50d));
+		assertThat(packageTest.toString(), is("2\n_\n1,2"));
+		packageTest.addAll(Arrays.asList(new PackageItem().withCostAs(10d)
+				.withIndexAs(3)
+				.withWeightAs(20d),new PackageItem().withCostAs(11d)
+				.withIndexAs(4)
+				.withWeightAs(30d)));
+		assertThat(packageTest.toString(), is("4\n_\n1,2\n3,4"));
 	}
 
 }
