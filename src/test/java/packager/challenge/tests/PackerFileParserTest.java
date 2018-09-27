a3dd0403f216f7b24f2b4918941a0d4485a28967
Kwaku Twumasi-Afriyie <kwaku.twumasi@quakearts.com>
2018-09-27 08:59:25 +0000

    Refactor invalid file parser test to file parser test

--- src/test/java/packager/challenge/tests/PackerFileParserTest.java
+++ src/test/java/packager/challenge/tests/PackerFileParserTest.java
@@ -1,14 +1,139 @@
 package packager.challenge.tests;
 
-import static org.junit.Assert.*;
+import java.io.File;
 
 import org.junit.Test;
 
+import com.mobiquityinc.exception.APIException;
+import com.mobiquityinc.packer.fileparser.impl.PackerFileParserImpl;
+
 public class PackerFileParserTest {
 
-	@Test
-	public void testParse() {
-		fail("Not yet implemented");
+	PackerFileParserImpl parserImpl = new PackerFileParserImpl();
+	
+	@Test(expected=APIException.class)
+	public void testParseEmpty() throws Exception {
+		parserImpl.parse("src"+File.separator+"test"
+				+File.separator+"resources"
+				+File.separator+"testEmpty.parserfile");
+	}
+	
+	@Test(expected=APIException.class)
+	public void testParseEmptyIndex() throws Exception {
+		parserImpl.parse("src"+File.separator+"test"
+				+File.separator+"resources"
+				+File.separator+"testEmptyIndex.parserfile");
 	}
 
+	@Test(expected=APIException.class)
+	public void testParseEmptyPackageLimit() throws Exception {
+		parserImpl.parse("src"+File.separator+"test"
+				+File.separator+"resources"
+				+File.separator+"testEmptyPackageLimit.parserfile");
+	}
+	
+	@Test(expected=APIException.class)
+	public void testParseEmptyPrice() throws Exception {
+		parserImpl.parse("src"+File.separator+"test"
+				+File.separator+"resources"
+				+File.separator+"testEmptyPrice.parserfile");
+	}
+
+	@Test(expected=APIException.class)
+	public void testParseEmptyWeight() throws Exception {
+		parserImpl.parse("src"+File.separator+"test"
+				+File.separator+"resources"
+				+File.separator+"testEmptyWeight.parserfile");
+	}
+
+	@Test(expected=APIException.class)
+	public void testParseInvalidEntry() throws Exception {
+		parserImpl.parse("src"+File.separator+"test"
+				+File.separator+"resources"
+				+File.separator+"testInvalidEntry.parserfile");
+	}
+
+	@Test(expected=APIException.class)
+	public void testParseInvalidIndex() throws Exception {
+		parserImpl.parse("src"+File.separator+"test"
+				+File.separator+"resources"
+				+File.separator+"testInvalidIndex.parserfile");
+	}
+	
+	@Test(expected=APIException.class)
+	public void testParseInvalidPackageLimit1() throws Exception {
+		parserImpl.parse("src"+File.separator+"test"
+				+File.separator+"resources"
+				+File.separator+"testInvalidPackageLimit1.parserfile");
+	}
+
+	@Test(expected=APIException.class)
+	public void testParseInvalidPackageLimit2() throws Exception {
+		parserImpl.parse("src"+File.separator+"test"
+				+File.separator+"resources"
+				+File.separator+"testInvalidPackageLimit2.parserfile");
+	}
+	
+	@Test(expected=APIException.class)
+	public void testParseInvalidPrice1() throws Exception {
+		parserImpl.parse("src"+File.separator+"test"
+				+File.separator+"resources"
+				+File.separator+"testInvalidPrice1.parserfile");
+	}
+
+	@Test(expected=APIException.class)
+	public void testParseInvalidPrice2() throws Exception {
+		parserImpl.parse("src"+File.separator+"test"
+				+File.separator+"resources"
+				+File.separator+"testInvalidPrice2.parserfile");
+	}
+	
+	@Test(expected=APIException.class)
+	public void testParseInvalidPrice3() throws Exception {
+		parserImpl.parse("src"+File.separator+"test"
+				+File.separator+"resources"
+				+File.separator+"testInvalidPrice3.parserfile");
+	}
+
+	@Test(expected=APIException.class)
+	public void testParseInvalidPrice4() throws Exception {
+		parserImpl.parse("src"+File.separator+"test"
+				+File.separator+"resources"
+				+File.separator+"testInvalidPrice4.parserfile");
+	}
+
+	@Test(expected=APIException.class)
+	public void testParseMissingEntry() throws Exception {
+		parserImpl.parse("src"+File.separator+"test"
+				+File.separator+"resources"
+				+File.separator+"testMissingEntry.parserfile");
+	}
+
+	@Test(expected=APIException.class)
+	public void testParseMissingIndex() throws Exception {
+		parserImpl.parse("src"+File.separator+"test"
+				+File.separator+"resources"
+				+File.separator+"testMissingIndex.parserfile");
+	}
+	
+	@Test(expected=APIException.class)
+	public void testParseMissingPackageLimit() throws Exception {
+		parserImpl.parse("src"+File.separator+"test"
+				+File.separator+"resources"
+				+File.separator+"testMissingPackageLimit.parserfile");
+	}
+
+	@Test(expected=APIException.class)
+	public void testParseMissingPrice() throws Exception {
+		parserImpl.parse("src"+File.separator+"test"
+				+File.separator+"resources"
+				+File.separator+"testMissingPrice.parserfile");
+	}
+	
+	@Test(expected=APIException.class)
+	public void testParseMissingWeight() throws Exception {
+		parserImpl.parse("src"+File.separator+"test"
+				+File.separator+"resources"
+				+File.separator+"testMissingWeight.parserfile");
+	}
 }
