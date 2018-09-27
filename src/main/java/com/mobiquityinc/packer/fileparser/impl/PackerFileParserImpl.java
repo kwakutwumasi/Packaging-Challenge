51895874e4a43ab9cca986b99769476337e8e454
Kwaku Twumasi-Afriyie <kwaku.twumasi@quakearts.com>
2018-09-27 16:23:34 +0000

    Modify test and parser implementation to check for right error messages

--- src/main/java/com/mobiquityinc/packer/fileparser/impl/PackerFileParserImpl.java
+++ src/main/java/com/mobiquityinc/packer/fileparser/impl/PackerFileParserImpl.java
@@ -29,73 +29,74 @@ public class PackerFileParserImpl implements PackerFileParser {
 		BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
 		List<PackerFileEntry> entries = new ArrayList<>();
 		
+		String line;
+		int count=0;
+		while ((line = reader.readLine())!=null) {
+			processLine(line, entries,++count);
+		}
+		
 		if(entries.isEmpty())
 			throw new APIException(Messages.get("empty.file"));
-		
-		String line;
-		while ((line = reader.readLine())!=null) {
-			processLine(line, entries);
-		}
 		
 		return entries.toArray(new PackerFileEntry[entries.size()]);
 	}
 
-	private void processLine(String line, List<PackerFileEntry> entries) throws APIException {
+	private void processLine(String line, List<PackerFileEntry> entries,int lineCount) throws APIException {
 		String[] lineParts = line.split("\\s+:\\s+",2);
 		Double weightLimit; 
 		try {
 			weightLimit = Double.parseDouble(lineParts[0]);
 		} catch (NumberFormatException e) {
-			throw new APIException(MessageFormat.format(Messages.get("invalid.weight.limit"), lineParts[0]));
+			throw new APIException(MessageFormat.format(Messages.get("invalid.weight.limit"), line, lineCount));
 		}
 		
 		if(lineParts.length!=2)
-			throw new APIException(MessageFormat.format(Messages.get("invalid.line"), line));
+			throw new APIException(MessageFormat.format(Messages.get("invalid.line"), line, lineCount));
 		
 		PackerFileEntry entry = new PackerFileEntry().withWeightLimitAs(weightLimit);
 		entries.add(entry);
-		processEntries(entry, lineParts[2]);
+		processEntries(entry, lineParts[1], lineCount);
 	}
 
-	private void processEntries(PackerFileEntry entry, String itemsString) throws APIException {
+	private void processEntries(PackerFileEntry entry, String itemsString, int lineCount) throws APIException {
 		String[] items = itemsString.split("\\s+");
 		for(String item:items) {
 			if(item.startsWith("(")
 					&& item.endsWith(")")) {				
-				processEntry(entry,item.substring(1,item.length()-1));
+				processEntry(entry,item.substring(1,item.length()-1), lineCount);
 			} else {
-				throw new APIException(MessageFormat.format(Messages.get("invalid.item"), item));
+				throw new APIException(MessageFormat.format(Messages.get("invalid.item"), item, lineCount));
 			}
 		}
 	}
 
-	private void processEntry(PackerFileEntry entry, String item) throws APIException {
+	private void processEntry(PackerFileEntry entry, String item, int lineCount) throws APIException {
 		String[] itemParts = item.split(",",3);
 		if(itemParts.length!=3)
-			throw new APIException(MessageFormat.format(Messages.get("invalid.item"), item));
+			throw new APIException(MessageFormat.format(Messages.get("invalid.item"), item, lineCount));
 			
 		int index;
 		try {
 			index = Integer.parseInt(itemParts[0]);
 		} catch (NumberFormatException e) {
-			throw new APIException(MessageFormat.format(Messages.get("invalid.index"), itemParts[0]));
+			throw new APIException(MessageFormat.format(Messages.get("invalid.index.file"), item, lineCount,e.getMessage()));
 		}
 		
 		double weight;
 		try {
 			weight = Double.parseDouble(itemParts[1]);
 		} catch (NumberFormatException e) {
-			throw new APIException(MessageFormat.format(Messages.get("invalid.weight"), itemParts[1]));
+			throw new APIException(MessageFormat.format(Messages.get("invalid.weight"), item, lineCount));
 		}
 
 		if(itemParts[2].length()<2) {
-			throw new APIException(MessageFormat.format(Messages.get("invalid.cost"), itemParts[2]));
+			throw new APIException(MessageFormat.format(Messages.get("invalid.cost"), item, lineCount));
 		}
 			
 		String currency = itemParts[2].substring(0, 1);
 		
 		if(!currencyMap.containsKey(currency))
-			throw new APIException(MessageFormat.format(Messages.get("invalid.currency"), currency));
+			throw new APIException(MessageFormat.format(Messages.get("invalid.currency"), item, lineCount));
 		
 		String costString = itemParts[2].substring(1);
 		
@@ -103,14 +104,19 @@ public class PackerFileParserImpl implements PackerFileParser {
 		try {
 			cost = Double.parseDouble(costString);
 		} catch (NumberFormatException e) {
-			throw new APIException(MessageFormat.format(Messages.get("invalid.cost"), costString));
+			throw new APIException(MessageFormat.format(Messages.get("invalid.cost"), item, lineCount));
 		}
 
-		entry.add(new PackageItem()
-				.withIndexAs(index)
-				.withWeightAs(weight)
-				.withCurrencyAs(currency)
-				.withCostAs(cost));
+		try {			
+			entry.add(new PackageItem()
+					.withIndexAs(index)
+					.withWeightAs(weight)
+					.withCurrencyAs(currency)
+					.withCostAs(cost));
+		} catch (APIException e) {
+			throw new APIException(MessageFormat.format(Messages.get("invalid.index.file"), item, lineCount, 
+					e.getMessage()));			
+		}
 	}
 
 }
--- src/test/java/packager/challenge/tests/PackerFileParserTest.java
+++ src/test/java/packager/challenge/tests/PackerFileParserTest.java
@@ -6,7 +6,9 @@ import static org.hamcrest.core.Is.*;
 import java.io.ByteArrayInputStream;
 import java.io.File;
 
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
 
 import com.mobiquityinc.exception.APIException;
 import com.mobiquityinc.packer.fileparser.impl.PackerFileParserImpl;
@@ -16,12 +18,15 @@ public class PackerFileParserTest {
 
 	PackerFileParserImpl parserImpl = new PackerFileParserImpl();
 	
+	@Rule
+	public ExpectedException expectedException = ExpectedException.none();
+	
 	@Test
 	public void testParser() throws Exception {
 		PackerFileEntry[] entries = parserImpl.parse("src"+File.separator+"test"
 				+File.separator+"resources"
 				+File.separator+"test.parserfile");
-		assertThat(entries.length, is(6));
+		assertThat(entries.length, is(9));
 		assertThat(entries[0].getWeightLimit(), is(81d));
 		assertThat(entries[0].getPackageItems().size(), is(6));
 		assertThat(entries[0].getPackageItems().get(0).getIndex(), is(1));
@@ -30,154 +35,229 @@ public class PackerFileParserTest {
 		assertThat(entries[0].getPackageItems().get(0).getWeight(), is(53.38d));
 		//No need to test each entry. Just check the last entry. If the last entry is correct,
 		//then all entries would have been parsed
-		assertThat(entries[5].getWeightLimit(), is(50d));
-		assertThat(entries[5].getPackageItems().size(), is(4));
-		assertThat(entries[5].getPackageItems().get(3).getIndex(), is(4));
-		assertThat(entries[5].getPackageItems().get(3).getCost(), is(70d));
-		assertThat(entries[5].getPackageItems().get(3).getCurrency(), is("€"));
-		assertThat(entries[5].getPackageItems().get(3).getWeight(), is(8.70d));
+		assertThat(entries[8].getWeightLimit(), is(50.80d));
+		assertThat(entries[8].getPackageItems().size(), is(4));
+		assertThat(entries[8].getPackageItems().get(3).getIndex(), is(4));
+		assertThat(entries[8].getPackageItems().get(3).getCost(), is(100d));
+		assertThat(entries[8].getPackageItems().get(3).getCurrency(), is("€"));
+		assertThat(entries[8].getPackageItems().get(3).getWeight(), is(91d));
 	}
 	
 	//Test some parser edge cases	
-	@Test(expected=APIException.class)
+	@Test
 	public void testSpacedEntries() throws Exception {
+		expectedException.expect(APIException.class);
+		expectedException.expectMessage(is("The item entry could not be parsed. "
+				+ "Ensure that each entry follows the format '(INDEX, WEIGHT, COST)' where WEIGHT is a valid number, "
+				+ "INDEX is a valid integer and COST is of the format CURRENCYSYMBOL+AMOUNT, ex $20. The error occurred on line 1 (()"));
 		ByteArrayInputStream testStream = new ByteArrayInputStream("81 :	( 1 ,  53.38	,€45 )".getBytes());
 		parserImpl.parse(testStream);
 	}
 
-	@Test(expected=APIException.class)
+	@Test
 	public void testUnSpacedLine() throws Exception {
+		expectedException.expect(APIException.class);
+		expectedException.expectMessage(is("The weight limit could not be parsed. Ensure that each line follows the format "
+				+ "'WEIGHTLIMIT : (INDEX, WEIGHT, COST) (INDEX, WEIGHT, COST) ...' where WEIGHTLIMIT and WEIGHT is a valid number, "
+				+ "INDEX is a valid integer and COST is of the format CURRENCYSYMBOL+AMOUNT, ex $20. "
+				+ "The error occurred on line 1 (81:(1,53.38,€45)(2,88.62,€98))"));
 		ByteArrayInputStream testStream = new ByteArrayInputStream("81:(1,53.38,€45)(2,88.62,€98)".getBytes());
 		parserImpl.parse(testStream);
 	}
 
-	@Test(expected=APIException.class)
+	@Test
 	public void testEmptyMissingEntries() throws Exception {
+		expectedException.expect(APIException.class);
+		expectedException.expectMessage(is("A file line could not be parsed. Ensure that each line follows the format "
+				+ "'WEIGHTLIMIT : (INDEX, WEIGHT, COST) (INDEX, WEIGHT, COST) ...' where WEIGHTLIMIT and WEIGHT is a "
+				+ "valid number, INDEX is a valid integer and COST is of the format CURRENCYSYMBOL+AMOUNT, ex $20. "
+				+ "The error occurred on line 1 (81)"));
 		ByteArrayInputStream testStream = new ByteArrayInputStream("81".getBytes());
 		parserImpl.parse(testStream);
 	}
 
-	@Test(expected=APIException.class)
+	@Test
 	public void testParseEmpty() throws Exception {
+		expectedException.expect(APIException.class);
+		expectedException.expectMessage(is("The file passed to the pack method contained no entries"));
 		parserImpl.parse("src"+File.separator+"test"
 				+File.separator+"resources"
 				+File.separator+"testEmpty.parserfile");
 	}
 	
-	@Test(expected=APIException.class)
+	@Test
 	public void testParseEmptyIndex() throws Exception {
+		expectedException.expect(APIException.class);
+		expectedException.expectMessage(is("Invalid index for entry on line 1 (,53.38,€45). For input string: \"\""));
 		parserImpl.parse("src"+File.separator+"test"
 				+File.separator+"resources"
 				+File.separator+"testEmptyIndex.parserfile");
 	}
 
-	@Test(expected=APIException.class)
+	@Test
 	public void testParseEmptyPackageLimit() throws Exception {
+		expectedException.expect(APIException.class);
+		expectedException.expectMessage(is("The weight limit could not be parsed. Ensure that each line follows "
+				+ "the format 'WEIGHTLIMIT : (INDEX, WEIGHT, COST) (INDEX, WEIGHT, COST) ...' where WEIGHTLIMIT "
+				+ "and WEIGHT is a valid number, INDEX is a valid integer and COST is of the format CURRENCYSYMBOL+AMOUNT, ex $20. "
+				+ "The error occurred on line 1 (: (1,53.38,€45) (2,88.62,€98) (3,72.30,€76) (4,30.18,€9) (5,46.34,€48))"));
 		parserImpl.parse("src"+File.separator+"test"
 				+File.separator+"resources"
 				+File.separator+"testEmptyPackageLimit.parserfile");
 	}
 	
-	@Test(expected=APIException.class)
+	@Test
 	public void testParseEmptyPrice() throws Exception {
+		expectedException.expect(APIException.class);
+		expectedException.expectMessage(is("The cost entry could not be parsed. Ensure that it is of the format "
+				+ "CURRENCYSYMBOL+AMOUNT, ex $20. The error occurred on line 1 (1,53.38,)"));
 		parserImpl.parse("src"+File.separator+"test"
 				+File.separator+"resources"
 				+File.separator+"testEmptyPrice.parserfile");
 	}
 
-	@Test(expected=APIException.class)
+	@Test
 	public void testParseEmptyWeight() throws Exception {
+		expectedException.expect(APIException.class);
+		expectedException.expectMessage(is("The weight entry could not be parsed. Ensure that it is a valid number. "
+				+ "The error occurred on line 1 (1,,53.38)"));
 		parserImpl.parse("src"+File.separator+"test"
 				+File.separator+"resources"
 				+File.separator+"testEmptyWeight.parserfile");
 	}
 
-	@Test(expected=APIException.class)
+	@Test
 	public void testParseInvalidEntry() throws Exception {
+		expectedException.expect(APIException.class);
+		expectedException.expectMessage(is("The item entry could not be parsed. Ensure that each entry follows the format "
+				+ "'(INDEX, WEIGHT, COST)' where WEIGHT is a valid number, INDEX is a valid integer and COST is of the "
+				+ "format CURRENCYSYMBOL+AMOUNT, ex $20. The error occurred on line 1 (53.38,1,€45)"));
 		parserImpl.parse("src"+File.separator+"test"
 				+File.separator+"resources"
 				+File.separator+"testInvalidEntry.parserfile");
 	}
 
-	@Test(expected=APIException.class)
+	@Test
 	public void testParseInvalidIndex() throws Exception {
+		expectedException.expect(APIException.class);
+		expectedException.expectMessage(is("Invalid index for entry on line 1 (Hello,53.38,€45). For input string: \"Hello\""));
 		parserImpl.parse("src"+File.separator+"test"
 				+File.separator+"resources"
 				+File.separator+"testInvalidIndex.parserfile");
 	}
 	
-	@Test(expected=APIException.class)
+	@Test
 	public void testParseInvalidPackageLimit1() throws Exception {
+		expectedException.expect(APIException.class);
+		expectedException.expectMessage(is("The weight limit could not be parsed. Ensure that each line follows the format "
+				+ "'WEIGHTLIMIT : (INDEX, WEIGHT, COST) (INDEX, WEIGHT, COST) ...' where WEIGHTLIMIT and WEIGHT is a valid "
+				+ "number, INDEX is a valid integer and COST is of the format CURRENCYSYMBOL+AMOUNT, ex $20. The error occurred "
+				+ "on line 1 (Hello : (1,53.38,€45) (2,88.62,€98) (3,78.48,€3) (4,72.30,€76) (5,30.18,€9) (6,46.34,€48))"));
 		parserImpl.parse("src"+File.separator+"test"
 				+File.separator+"resources"
 				+File.separator+"testInvalidPackageLimit1.parserfile");
 	}
 
-	@Test(expected=APIException.class)
+	@Test
 	public void testParseInvalidPackageLimit2() throws Exception {
+		expectedException.expect(APIException.class);
+		expectedException.expectMessage(is("The weight limit could not be parsed. Ensure that each line follows the format "
+				+ "'WEIGHTLIMIT : (INDEX, WEIGHT, COST) (INDEX, WEIGHT, COST) ...' where WEIGHTLIMIT and WEIGHT is a "
+				+ "valid number, INDEX is a valid integer and COST is of the format CURRENCYSYMBOL+AMOUNT, ex $20. "
+				+ "The error occurred on line 1 (81 (1,53.38,€45) (2,88.62,€98) (3,78.48,€3) (4,72.30,€76) (5,30.18,€9) "
+				+ "(6,46.34,€48))"));
 		parserImpl.parse("src"+File.separator+"test"
 				+File.separator+"resources"
 				+File.separator+"testInvalidPackageLimit2.parserfile");
 	}
 	
-	@Test(expected=APIException.class)
+	@Test
 	public void testParseInvalidPrice1() throws Exception {
+		expectedException.expect(APIException.class);
+		expectedException.expectMessage(is("The currency symbol was not recognized. The error occurred on line 1 (1,53.38,35)"));
 		parserImpl.parse("src"+File.separator+"test"
 				+File.separator+"resources"
 				+File.separator+"testInvalidPrice1.parserfile");
 	}
 
-	@Test(expected=APIException.class)
+	@Test
 	public void testParseInvalidPrice2() throws Exception {
+		expectedException.expect(APIException.class);
+		expectedException.expectMessage(is("The cost entry could not be parsed. Ensure that it is of the format CURRENCYSYMBOL+AMOUNT,"
+				+ " ex $20. The error occurred on line 1 (1,53.38,€)"));
 		parserImpl.parse("src"+File.separator+"test"
 				+File.separator+"resources"
 				+File.separator+"testInvalidPrice2.parserfile");
 	}
 	
-	@Test(expected=APIException.class)
+	@Test
 	public void testParseInvalidPrice3() throws Exception {
+		expectedException.expect(APIException.class);
+		expectedException.expectMessage(is("The currency symbol was not recognized. The error occurred on line 1 (1,53.38,\"€34\")"));
 		parserImpl.parse("src"+File.separator+"test"
 				+File.separator+"resources"
 				+File.separator+"testInvalidPrice3.parserfile");
 	}
 
-	@Test(expected=APIException.class)
+	@Test
 	public void testParseInvalidPrice4() throws Exception {
+		expectedException.expect(APIException.class);
+		expectedException.expectMessage(is("The currency symbol was not recognized. The error occurred on line 1 (1,53.38,+34)"));
 		parserImpl.parse("src"+File.separator+"test"
 				+File.separator+"resources"
 				+File.separator+"testInvalidPrice4.parserfile");
 	}
 
-	@Test(expected=APIException.class)
+	@Test
 	public void testParseMissingEntry() throws Exception {
+		expectedException.expect(APIException.class);
+		expectedException.expectMessage(is("Invalid index for entry on line 1 (4,72.30,€76). "
+				+ "The index is not valid. Expected 3, but found 4"));
 		parserImpl.parse("src"+File.separator+"test"
 				+File.separator+"resources"
 				+File.separator+"testMissingEntry.parserfile");
 	}
 
-	@Test(expected=APIException.class)
+	@Test
 	public void testParseMissingIndex() throws Exception {
+		expectedException.expect(APIException.class);
+		expectedException.expectMessage(is("The item entry could not be parsed. Ensure that each entry follows the format "
+				+ "'(INDEX, WEIGHT, COST)' where WEIGHT is a valid number, INDEX is a valid integer and COST is of the "
+				+ "format CURRENCYSYMBOL+AMOUNT, ex $20. The error occurred on line 1 (53.38,€45)"));
 		parserImpl.parse("src"+File.separator+"test"
 				+File.separator+"resources"
 				+File.separator+"testMissingIndex.parserfile");
 	}
 	
-	@Test(expected=APIException.class)
+	@Test
 	public void testParseMissingPackageLimit() throws Exception {
+		expectedException.expect(APIException.class);
+		expectedException.expectMessage(is("The weight limit could not be parsed. Ensure that each line follows the format "
+				+ "'WEIGHTLIMIT : (INDEX, WEIGHT, COST) (INDEX, WEIGHT, COST) ...' where WEIGHTLIMIT and WEIGHT is a valid "
+				+ "number, INDEX is a valid integer and COST is of the format CURRENCYSYMBOL+AMOUNT, ex $20. The error "
+				+ "occurred on line 1 (: (1,53.38,€45) (2,88.62,€98) (3,78.48,€3) (4,72.30,€76) (5,30.18,€9) (6,46.34,€48))"));
 		parserImpl.parse("src"+File.separator+"test"
 				+File.separator+"resources"
 				+File.separator+"testMissingPackageLimit.parserfile");
 	}
 
-	@Test(expected=APIException.class)
+	@Test
 	public void testParseMissingPrice() throws Exception {
+		expectedException.expect(APIException.class);
+		expectedException.expectMessage(is("The item entry could not be parsed. Ensure that each entry follows the format "
+				+ "'(INDEX, WEIGHT, COST)' where WEIGHT is a valid number, INDEX is a valid integer and COST is of the "
+				+ "format CURRENCYSYMBOL+AMOUNT, ex $20. The error occurred on line 1 (1,53.38)"));
 		parserImpl.parse("src"+File.separator+"test"
 				+File.separator+"resources"
 				+File.separator+"testMissingPrice.parserfile");
 	}
 	
-	@Test(expected=APIException.class)
+	@Test
 	public void testParseMissingWeight() throws Exception {
+		expectedException.expect(APIException.class);
+		expectedException.expectMessage(is("The item entry could not be parsed. Ensure that each entry follows the format "
+				+ "'(INDEX, WEIGHT, COST)' where WEIGHT is a valid number, INDEX is a valid integer and COST is of the "
+				+ "format CURRENCYSYMBOL+AMOUNT, ex $20. The error occurred on line 1 (1,53.38)"));
 		parserImpl.parse("src"+File.separator+"test"
 				+File.separator+"resources"
 				+File.separator+"testMissingWeight.parserfile");
