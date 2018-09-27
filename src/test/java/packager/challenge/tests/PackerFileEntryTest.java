26bc9edbee4384bd8d7eeb825ebc9fdf0cf5db3a
Kwaku Twumasi-Afriyie <kwaku.twumasi@quakearts.com>
2018-09-27 09:36:21 +0000

    Add packer file entry test

--- src/test/java/packager/challenge/tests/PackerFileEntryTest.java
+++ src/test/java/packager/challenge/tests/PackerFileEntryTest.java
@@ -4,36 +4,34 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
+import com.mobiquityinc.packer.model.PackageItem;
+import com.mobiquityinc.packer.model.PackerFileEntry;
+
 public class PackerFileEntryTest {
 
 	@Test
-	public void testHashCode() {
-		fail("Not yet implemented");
+	public void testHashCodeEquals() {
+		PackageItem item1 = new PackageItem().withCostAs(10d)
+				.withIndexAs(1)
+				.withWeightAs(20d),
+				item2 = new PackageItem().withCostAs(11d)
+				.withIndexAs(2)
+				.withWeightAs(30d);
+		
+		PackerFileEntry entry1 = new PackerFileEntry().withWeightLimitAs(74)
+				.add(item1)
+				.add(item2),
+				entry2 = new PackerFileEntry().withWeightLimitAs(74)
+				.add(item1)
+				.add(item2);
+		assertEquals(entry1, entry1);
+		assertEquals(entry1, entry2);
+		assertEquals(entry1.hashCode(), entry2.hashCode());
+		assertFalse(entry1.equals(null));
+		assertFalse(entry1.equals(new PackerFileEntry() {}));
+		assertFalse(entry1.equals(new PackerFileEntry()));
+		assertFalse(entry1.equals(new PackerFileEntry().withWeightLimitAs(11)
+				.add(item1)
+				.add(item2)));
 	}
-
-	@Test
-	public void testGetWeightLimit() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	public void testSetWeightLimit() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	public void testWithWeightLimitAs() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	public void testGetPackageItems() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	public void testEqualsObject() {
-		fail("Not yet implemented");
-	}
-
 }
